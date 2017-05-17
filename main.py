b9413dcb7dfeace8eccfb73c80902765910ac25c
Benoit Bourdon <chinchifou@gmail.com>
2017-05-17 18:07:40 +0200

    Fix display of the menu

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pygame.locals import *
 import config as cfg
 from config import *
 
+from math import floor
 
 
 #INITIALIZATION
@@ -20,14 +21,13 @@ TILE_PER_BOARD_COLUMN = 15 #NEVER CHANGE
 
 TILE_SIZE = 60 #MUST DIVIDE BTH 1920 and 1080 / for instance 12; 15; 20; 24; 30; 40; 60 work
 
-TILE_WIDTH_SCREEN = 1920 / TILE_SIZE
-TILE_HEIGH_SCREEN = 1080 / TILE_SIZE
+WIDTH_SCREEN_IN_TILES = 1920 / TILE_SIZE
+HEIGH_SCREEN_IN_TILES = 1080 / TILE_SIZE
 
+BOARD_SIZE_IN_TILES = 18
 
-FOOTER_WIDTH = TILE_PER_BOARD_COLUMN * TILE_SIZE
-FOOTER_HEIGH = ( TILE_HEIGH_SCREEN - TILE_PER_BOARD_COLUMN ) * TILE_SIZE
-MENU_WIDTH = ( TILE_WIDTH_SCREEN - TILE_PER_BOARD_COLUMN ) * TILE_SIZE
-MENU_HEIGH = TILE_HEIGH_SCREEN * TILE_SIZE
+MENU_WIDTH_IN_TILES = WIDTH_SCREEN_IN_TILES - BOARD_SIZE_IN_TILES
+MENU_HEIGH_IN_TILES = HEIGH_SCREEN_IN_TILES
 
 # 0 : start
 # 1 : normal tile
@@ -57,17 +57,20 @@ LAYOUT = [
 
 #CHANGING WITH WINDOW RESIZING
 ratio = float(cfg.WIDTH / 1920) #reference resolution is 1920*1080
+delta = 1.5 * TILE_SIZE #distance of board from top left corner
 window =  pygame.display.set_mode( (0, 0) )
 tile_size = round(TILE_SIZE * ratio)
-footer_width = round(FOOTER_WIDTH * ratio)
-footer_heigh = round(FOOTER_HEIGH * ratio)
-menu_width = round(MENU_WIDTH * ratio)
-menu_heigh = round(MENU_HEIGH * ratio)
+board_size = round(BOARD_SIZE_IN_TILES * tile_size)
+menu_width = round(MENU_WIDTH_IN_TILES * tile_size)
+menu_heigh = round(MENU_HEIGH_IN_TILES * tile_size)
 
 
 
 #IMAGES LOADING
 print ('    Loading images...')
+
+tile_start = pygame.image.load("./images/tile_start.png")
+
 tile = pygame.image.load("./images/tile.png") # TODO change path for Linux ?
 
 tile_double_letter = pygame.image.load("./images/tile_double_letter.png")
@@ -76,10 +79,10 @@ tile_triple_letter = pygame.image.load("./images/tile_triple_letter.png")
 tile_double_word = pygame.image.load("./images/tile_double_word.png")
 tile_triple_word = pygame.image.load("./images/tile_triple_word.png")
 
-tile_start = pygame.image.load("./images/tile_start.png")
+board = pygame.image.load("./images/board.png")
 
-footer = pygame.image.load("./images/footer_textured.png")
 menu = pygame.image.load("./images/menu.png")
+
 print('    Images loaded')
 
 
@@ -111,8 +114,10 @@ def refreshWindow(window, width, heigh) :
 
 #Draw playing board
 def drawBoard() :
-    x_pos = 0
-    y_pos = 0
+    x_pos = 0 + delta
+    y_pos = 0 + delta
+
+    window.blit(board, (0, 0))
 
     for row in range(0,TILE_PER_BOARD_COLUMN) :
         for column in range(0, TILE_PER_BOARD_COLUMN) :
@@ -129,19 +134,22 @@ def drawBoard() :
             elif LAYOUT[row][column] == 5 :
                 window.blit(tile_triple_word,(x_pos, y_pos))
             x_pos += tile_size
-        x_pos = 0
+        x_pos = 0 + delta
         y_pos += tile_size
 
-    window.blit(footer, (0, TILE_PER_BOARD_COLUMN*tile_size))
-    window.blit(menu, (TILE_PER_BOARD_COLUMN*tile_size, 0))
+    window.blit(menu, (board_size, 0))
+
     pygame.display.flip()
 
 def updateRatio(width, heigh) :
     return min( float(width / 1920), float(heigh/1080) )
 
+def updateDelta() :
+    return 1.5 * tile_size
+
 #Resize tile    
 def updateTileSize() :
-    return round(TILE_SIZE * ratio) #TO IMPROVE, Based on tile size
+    return floor(TILE_SIZE * ratio) #TO IMPROVE, Based on tile size
 def resizeTile() :
     return pygame.transform.smoothscale(tile, (tile_size, tile_size) )
 def resizeTileDoubleLetter() :
@@ -155,22 +163,19 @@ def resizeTileTripleWord() :
 def resizeTileStart() :
     return pygame.transform.smoothscale(tile_start, (tile_size, tile_size) )    
 
-#Resize footer
-def updateFooterWidth() :
-    return round(TILE_PER_BOARD_COLUMN * tile_size)
-def updateFooterHeigh() :
-    return round(( TILE_HEIGH_SCREEN - TILE_PER_BOARD_COLUMN ) * tile_size)
-def resizeFooter() :
-    return pygame.transform.smoothscale(footer, (footer_width, footer_heigh) )
+#Resize board
+def updateBoardSize() :
+    return round( HEIGH_SCREEN_IN_TILES  * tile_size )
+def resizeBoard() :
+    return pygame.transform.smoothscale(board, (board_size, board_size) )
 
 #Resize menu
 def updateMenuWidth() :
-    return round(( TILE_WIDTH_SCREEN - TILE_PER_BOARD_COLUMN ) * tile_size)
+    return round( ( WIDTH_SCREEN_IN_TILES - BOARD_SIZE_IN_TILES  ) * tile_size)
 def updateMenuHeigh() :
-    return round(TILE_HEIGH_SCREEN * tile_size)
+    return round(HEIGH_SCREEN_IN_TILES * tile_size)
 def resizeMenu() :
     return pygame.transform.smoothscale(menu, (menu_width, menu_heigh) )
-
 
 
 
@@ -178,11 +183,11 @@ def resizeMenu() :
 window = refreshWindow(window, cfg.WIDTH, cfg.HEIGH)
 
 tile_size = updateTileSize()
+delta = updateDelta()
 tile = resizeTile()
 
-footer_width = updateMenuWidth()
-footer_heigh = updateFooterHeigh()
-footer = resizeFooter()
+board_size = updateBoardSize()
+board = resizeBoard()
 
 menu_width = updateMenuWidth()
 menu_heigh = updateMenuHeigh()
@@ -206,18 +211,20 @@ while running:
 
         elif (cfg.FULLSCREEN == False and cfg.RESIZABLE == True and event.type == VIDEORESIZE) :
             window = refreshWindow(window, event.dict['size'][0], event.dict['size'][1])
+            tile_start = pygame.image.load("./images/tile_start.png")
             tile = pygame.image.load("./images/tile.png") #to regain quality
             tile_double_letter = pygame.image.load("./images/tile_double_letter.png")
             tile_triple_letter = pygame.image.load("./images/tile_triple_letter.png")
             tile_double_word = pygame.image.load("./images/tile_double_word.png")
             tile_triple_word = pygame.image.load("./images/tile_triple_word.png")
-            tile_start = pygame.image.load("./images/tile_start.png")
-            footer = pygame.image.load("./images/footer_textured.png")
+            
+            board = pygame.image.load("./images/board.png") 
             menu = pygame.image.load("./images/menu.png") 
 
             ratio = updateRatio(event.dict['size'][0], event.dict['size'][1])
 
             tile_size = updateTileSize()
+            delta = updateDelta()
             tile = resizeTile()
             tile_double_letter = resizeTileDoubleLetter()
             tile_triple_letter = resizeTileTripleLetter()
@@ -225,9 +232,8 @@ while running:
             tile_triple_word = resizeTileTripleWord()
             tile_start = resizeTileStart()
 
-            footer_width = updateMenuWidth()
-            footer_heigh = updateFooterHeigh()
-            footer = resizeFooter()
+            board_size = updateBoardSize()
+            board = resizeBoard()
 
             menu_width = updateMenuWidth()
             menu_heigh = updateMenuHeigh()
